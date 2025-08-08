c3b61a0074dfeab89b0aeb1851a37d3c9158e06c
Chia-Hao Tung <71302963+tungchuw@users.noreply.github.com>
2025-08-08 10:07:30 -0500

    Update

--- scripts/Fig.1C_getHeatmap_chiadrop_PC3DM.sh
+++ scripts/Fig.1C_getHeatmap_chiadrop_PC3DM.sh
@@ -9,12 +9,12 @@
 #$ -e PC3.sge.e
 
 ##################
-singularity run /net/nwgc/vol1/nobackup/nocleanup/tungch/singularity/deeptools_3.3.2.sif computeMatrix \
+singularity run ./singularity/deeptools_3.3.2.sif computeMatrix \
     reference-point --referencePoint center -R ACD0013_mergeFrags.bed -S ACD0013.R1.q30.lenBE50.MAJOR_CHROM.bw ACD0014.R1.q30.lenBE50.MAJOR_CHROM.bw A0038.all_treat_pileup.bw A0015.all_treat_pileup.bw \
     -p 16 --beforeRegionStartLength 5000 --afterRegionStartLength 5000 --missingDataAsZero --skipZeros --blackListFileName hg38-blacklist.v2.bed \
     -o PC3.chiadrop_med1_H3K27ac_5000.gz
 
-singularity run /net/nwgc/vol1/nobackup/nocleanup/tungch/singularity/deeptools_3.3.2.sif plotHeatmap -m PC3.chiadrop_med1_H3K27ac_5000.gz \
+singularity run ./singularity/deeptools_3.3.2.sif plotHeatmap -m PC3.chiadrop_med1_H3K27ac_5000.gz \
     -out PC3.chiadrop_med1_H3K27ac_5000.heatmap.pdf --colorMap RdYlBu_r RdYlBu_r RdYlBu_r RdYlBu_r --samplesLabel "ACD0013" "ACD0014" "MED1" "H3K27Ac" \
     --whatToShow 'heatmap and colorbar' --regionsLabel "" \
     --zMax 22 21 0.8 0.8 --sortRegions descend --sortUsingSamples 1
